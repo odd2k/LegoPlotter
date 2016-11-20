c46a278d7873a214f52b5b6c8b7415161e807933
Oddbjørn <olsen.oddbjorn@gmail.com>
2016-11-20 16:38:09 +0100

    byttet akser i plotterklient (trodde denne var lasta opp?)

--- Legoplotter/src/PlotterKlient.java
+++ Legoplotter/src/PlotterKlient.java
@@ -4,28 +4,27 @@ import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.NXTTouchSensor;
-import lejos.hardware.sensor.EV3TouchSensor;
 
 public class PlotterKlient{
 	
 	static Plotter plotter;
 	
 	public static void main (String[] args) throws Exception{
-		NXTRegulatedMotor motorX = new NXTRegulatedMotor(MotorPort.A);
-		NXTRegulatedMotor motorY = new NXTRegulatedMotor(MotorPort.B);
-		NXTRegulatedMotor motorY2 = new NXTRegulatedMotor(MotorPort.C);
-		EV3LargeRegulatedMotor motorZ = null;
+		NXTRegulatedMotor motorY = new NXTRegulatedMotor(MotorPort.A);
+		NXTRegulatedMotor motorX = new NXTRegulatedMotor(MotorPort.B);
+		NXTRegulatedMotor motorX2 = new NXTRegulatedMotor(MotorPort.C);
+		EV3LargeRegulatedMotor motorZ = new EV3LargeRegulatedMotor(MotorPort.D);
 		NXTTouchSensor endestoppX = new NXTTouchSensor(SensorPort.S2);
+		NXTTouchSensor endestoppX2 = new NXTTouchSensor(SensorPort.S3);
 		NXTTouchSensor endestoppY = new NXTTouchSensor(SensorPort.S1);
-		EV3TouchSensor endestoppZ = null;
 		double hjulDiameter = 39.46;// I millimeter
 		
-		plotter = new Plotter(motorX, motorY, motorY2, motorZ, endestoppX, endestoppY, endestoppZ, hjulDiameter, 24.0f/40.0f, 1.0f/2.0f);
+		plotter = new Plotter(motorY, motorX, motorX2, motorZ, endestoppX, endestoppX2, endestoppY, hjulDiameter, 1.0f/2.0f, 24.0f/40.0f);
 
-		System.out.println("Venter pa klient...");
-		
-		plotter.utforKommandoer(KommandoListe.getKommandoListe(Plotter.PORTNR));
-		
-		Button.ENTER.waitForPressAndRelease();
+		while(true){
+			System.out.println("Venter pa klient...");
+			
+			plotter.utforKommandoer(KommandoListe.getKommandoListe(Plotter.PORTNR));
+		}
 	}
 }
